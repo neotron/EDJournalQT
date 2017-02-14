76081fb0820c8ae0b86c030b64bf73d1a1446672
David Hedbor <david@ookla.com>
2017-02-13 23:33:40 -0800

    Revert "add lib cmake file"
    This reverts commit 30f8cbd26b17840dbc2b4d85338eabf6030dfc22.

--- cmake/lib.cmake
+++
@@ -1,9 +0,0 @@
-
-macro(EDJournalLibrary)
-find_package(Qt5Core REQUIRED)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-set(LINK_LIBS Qt5::Core )
-file(GLOB LIB_SOURCE_FILES src/*.cpp src/*.h src/*.ui)
-add_library(EDJournalQT ${LIB_SOURCE_FILES})
-target_link_libraries(EDJournalQT ${LINK_LIBS})
-endmacro()
